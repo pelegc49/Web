2e01669a770050ba492cad4508c6c7543da60c81
pelegc49 <pelegc49@gmail.com>
2025-05-17 13:53:48 +0300

    Refactor Header component layout and improve dark mode toggle functionality

--- Front/src/components/Header.jsx
+++ Front/src/components/Header.jsx
@@ -2,19 +2,20 @@ import React, {useState} from 'react';
 import {sun, moon, guest_user} from "../assets/svgs.jsx"
 import "../index.css"
 export default function Header(){
-
     const [darkMode, setDarkMode] = useState(false);
     return(
-        <header className={"flex justify-between text-white p-4 duration-200 ease-in-out "+ (darkMode ? "bg-gray-500" : "bg-gray-300")}>
-            <div className="cursor-pointer">
+        <header className={"p-4 flex justify-between text-white duration-200 ease-in-out "+ (darkMode ? "bg-gray-500" : "bg-gray-300")}>
+            <div className="cursor-pointer text-black font-bold text-2xl duration-300 ease-in-out hover:opacity-30">
                 Tx2Class
             </div>
             <div>
-                {/*light/dark mode*/}
-                <div className="cursor-pointer"
-                  onClick={() => setDarkMode(!darkMode)}>
-                  <img className="duration-200 ease-in-out hover:opacity-30" width="30px" src={darkMode ? moon : sun} alt={darkMode ? "Dark" : "Light"} />
-                    <img width="30px" src={guest_user} alt="UserPic" />
+                <div className="cursor-pointer flex items-center gap-1 pr-2">
+                    <img onClick={() => setDarkMode(!darkMode)}
+                         className="duration-300 ease-in-out hover:opacity-30"
+                         width="30px" src={darkMode ? moon : sun}
+                         alt={darkMode ? "Dark" : "Light"} />
+                    <img className="duration-300 ease-in-out hover:opacity-30" 
+                         width="30px" src={guest_user} alt="UserPic" />
                 </div>
             </div>
         </header>
