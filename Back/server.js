e4dd5090425d8b237882825f7e6856976ef3e488
Peleg Canfi <pelegc49@gmail.com>
2025-05-22 08:50:14 +0300

    Initialize backend with Express server and package.json setup

---
+++ Back/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/',(req,res)=>{
+    res.send('Server is responding');
+});
