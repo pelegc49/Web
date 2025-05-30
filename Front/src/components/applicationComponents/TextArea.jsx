176c76d0db4432a08f82704e0b607c3e2b427d4e
pelegc49 <pelegc49@gmail.com>
2025-05-30 18:59:53 +0300

    refactor: update Application component to manage state from location and simplify props handling feat: enhance TextArea component to update content based on initialValue prop feat: add mock project data to HistoryList for testing and demonstration

--- Front/src/components/applicationComponents/TextArea.jsx
+++ Front/src/components/applicationComponents/TextArea.jsx
@@ -1,20 +1,24 @@
-import React, {useState } from "react";
+import React, {useEffect, useState } from "react";
 import * as styles from "../../assets/Style";
 
 export default function TextArea({ onContentChange, placeholder, initialValue = "" ,children}) {
   const [content, setContent] = useState(initialValue);
+  useEffect(()=>{
+    setContent(initialValue)
+  },[initialValue])
+  console.log(content);
   const handleChange = (e) => {
     const newContent = e.target.value;
     setContent(newContent);
     // Transfer content to parent component if callback is provided
     if (onContentChange) {
-      onContentChange(newContent);
+      onContentChange(e.target.value);
     }
   };
-  
   return (
     <div className={styles.textAreaContainer}>
-      <textarea 
+      <textarea
+      
         className={styles.textArea}
         value={content}
         onChange={handleChange}
